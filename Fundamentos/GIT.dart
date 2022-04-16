7849943d76b859c692981dc3ac87b1da7b1bea46
Rafael Ramos <rafael.ramosrc@gmail.com>
2022-04-16 16:48:13 -0300

    Criei a primeira versão comandos git

---
+++ Fundamentos/GIT.dart
@@ -0,0 +1,9 @@
+/*
+git status - Verifica as alterações do repositorio.
+git add . - adciona os arquivos para o commit (o . significa que sera adicionado tudo)
+git commit -m "NOME DO COMMIT" 
+git push origin BRANCH 
+git checkout BRANCH - navega entres os branches
+git checkout -b BRANCH - cria um novo branch
+git log - mostra o historico de log
+gitk - abre o historico de versões
